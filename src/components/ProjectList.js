06b49d151b693ee738a0d5abc93a2a4700235d22
Mercy <maritimmercy8@gmail.com>
2025-04-10 20:01:33 +0300

    added key

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,18 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
       <div id="project-list">{/* render ProjectItem components here */}</div>
+      {projects.map(p => {
+        return <ProjectItem
+          key={p.id} 
+          name={p.name}
+          technologies={p.technologies}
+          about={p.about}
+        />
+      })}
     </div>
   );
 }
